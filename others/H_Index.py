6e173710dd91b3e6931cb281953fa56e28116844
SirChain <70142814+SpecStra@users.noreply.github.com>
2023-01-13 01:12:18 +0900

    Add files via upload

---
+++ others/H_Index.py
@@ -0,0 +1,12 @@
+import copy
+import heapq
+import itertools
+import re
+from itertools import combinations, permutations, islice
+from collections import defaultdict, OrderedDict, deque, Counter
+import math
+import datetime
+from dateutil.relativedelta import relativedelta
+
+
+def solution(citations):
